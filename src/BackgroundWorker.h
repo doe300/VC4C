0d2d485e75e4ac5874eb6b40d1ba14f1e9ed22e2
doe300 <stadeldani@web.de>
2018-03-30 13:01:14 +0200

    Moves BackgroundWorker to own source file

--- src/BackgroundWorker.h
+++ src/BackgroundWorker.h
@@ -7,8 +7,6 @@
 #ifndef BACKGROUND_WORKER_H
 #define BACKGROUND_WORKER_H
 
-#include "log.h"
-
 #include <exception>
 #include <functional>
 #include <vector>
@@ -20,7 +18,7 @@
 #include <thread>
 #endif
 
-namespace threading
+namespace vc4c
 {
     /*
      * Container for code-executions which can be run in parallel
@@ -35,54 +33,12 @@ namespace threading
     struct BackgroundWorker
     {
     public:
-        BackgroundWorker(const std::function<void()>& f, const std::string& name) :
-            name(name), functor(f), err(nullptr)
-#ifdef MULTI_THREADED
-            ,
-            runner()
-#endif
-        {
-#ifdef MULTI_THREADED
-            // we need thread-support, so load the pthread library dynamically (if it is not yet loaded)
-            void* handle = dlopen("libpthread.so.0", RTLD_GLOBAL | RTLD_LAZY);
-            if(handle == nullptr)
-            {
-                throw std::runtime_error(std::string("Error loading pthread library: ") + dlerror());
-            }
-#endif
-        }
+        BackgroundWorker(const std::function<void()>& f, const std::string& name);
 
-        void operator()()
-        {
-            const auto f = [this]() -> void {
-#ifdef MULTI_THREADED
-                prctl(PR_SET_NAME, name.data(), 0, 0, 0);
-#endif
-                try
-                {
-                    functor();
-                }
-                catch(const std::exception& e)
-                {
-                    logging::error() << "Background worker threw error: " << e.what() << logging::endl;
-                    this->err = std::current_exception();
-                }
-            };
-#ifdef MULTI_THREADED
-            runner = std::thread(f);
-#else
-            f();
-#endif
-        }
+        void operator()();
 
-        std::exception_ptr waitFor()
-        {
-#ifdef MULTI_THREADED
-            if(runner.joinable())
-                runner.join();
-#endif
-            return err;
-        }
+        std::exception_ptr waitFor();
+
         std::string name;
         std::function<void()> functor;
         std::exception_ptr err;
@@ -90,22 +46,9 @@ namespace threading
         std::thread runner;
 #endif
 
-        static void waitForAll(std::vector<BackgroundWorker>& worker)
-        {
-            std::exception_ptr err = nullptr;
-            for(BackgroundWorker& w : worker)
-            {
-                std::exception_ptr tmp = w.waitFor();
-                if(tmp)
-                    err = tmp;
-            }
-            // throwing the exception after all workers are finished solves the problem of terminating worker-threads
-            //-> which crashes the program
-            if(err)
-                std::rethrow_exception(err);
-        }
+        static void waitForAll(std::vector<BackgroundWorker>& worker);
     };
 
-} /* namespace threading */
+} /* namespace vc4c */
 
 #endif /* BACKGROUND_WORKER_H */
