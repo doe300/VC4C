doe300/VC4C

commit 394a5ca51c3ca2d414c618f998f76397828402f0

Remove constant load in loops (#56)
* Add initial code for remove constant load opt

* Implement tree construction

* Implement load immediate movement

* Fix

* Fix insert point

* Move some definition to header

* Fix struct name

* Fix insert point

* Fix moving instruction way

* Fix some problem

* Use getCFG instead of createCFG

* Fix result type of getCFG

* Fix insert point

* Add test file

src/optimization/Optimizer.cpp
@@ -141,6 +141,7 @@ const OptimizationPass optimizations::COMBINE_LITERAL_LOADS = OptimizationPass("
 const OptimizationPass optimizations::COMBINE_ROTATIONS = OptimizationPass("CombineRotations", combineVectorRotations, 100);
 const OptimizationPass optimizations::REMOVE_REDUNDANT_MOVES = OptimizationPass("RemoveRedundantMoves", eliminateRedundantMoves, 110);
 const OptimizationPass optimizations::ELIMINATE = OptimizationPass("EliminateDeadStores", eliminateDeadStore, 120);
+const OptimizationPass optimizations::REMOVE_CONSTANT_LOAD_IN_LOOPS = OptimizationPass("RemoveConstantLoadInLoops", removeConstantLoadInLoops, 125);
 const OptimizationPass optimizations::VECTORIZE = OptimizationPass("VectorizeLoops", vectorizeLoops, 130);
 const OptimizationPass optimizations::SPLIT_READ_WRITES = OptimizationPass("SplitReadAfterWrites", splitReadAfterWrites, 140);
 const OptimizationPass optimizations::REORDER = OptimizationPass("ReorderInstructions", reorderWithinBasicBlocks, 150);
@@ -150,7 +151,7 @@ const OptimizationPass optimizations::ADD_START_STOP_SEGMENT = OptimizationPass(
 const OptimizationPass optimizations::EXTEND_BRANCHES = OptimizationPass("ExtendBranches", extendBranches, 190);
 
 const std::set<OptimizationPass> optimizations::DEFAULT_PASSES = {
-		MAP_MEMORY_ACCESS, RUN_SINGLE_STEPS, /* SPILL_LOCALS, */ COMBINE_LITERAL_LOADS, RESOLVE_STACK_ALLOCATIONS, COMBINE_ROTATIONS, REMOVE_REDUNDANT_MOVES, ELIMINATE, VECTORIZE, SPLIT_READ_WRITES, REORDER, COMBINE, UNROLL_WORK_GROUPS, ADD_START_STOP_SEGMENT, EXTEND_BRANCHES
+		MAP_MEMORY_ACCESS, RUN_SINGLE_STEPS, /* SPILL_LOCALS, */ COMBINE_LITERAL_LOADS, RESOLVE_STACK_ALLOCATIONS, COMBINE_ROTATIONS, REMOVE_REDUNDANT_MOVES, ELIMINATE, VECTORIZE, SPLIT_READ_WRITES, REORDER, COMBINE, UNROLL_WORK_GROUPS, ADD_START_STOP_SEGMENT, EXTEND_BRANCHES, REMOVE_CONSTANT_LOAD_IN_LOOPS
 };
 
 Optimizer::Optimizer(const Configuration& config, const std::set<OptimizationPass>& passes) : config(config), passes(passes)
@@ -162,7 +163,7 @@ static void runOptimizationPasses(const Module& module, Method& method, const Co
     logging::debug() << "-----" << logging::endl;
     logging::info() << "Running optimization passes for: " << method.name << logging::endl;
     std::size_t numInstructions = method.countInstructions();
-    
+
     for(const OptimizationPass& pass : passes)
     {
         logging::debug() << logging::endl;

src/Graph.h
@@ -23,6 +23,7 @@ namespace vc4c
 	struct Node : private NonCopyable
 	{
 		using NeighborsType = FastMap<Node*, R>;
+		using KeyType = K;
 
 		const K key;
 

src/analysis/ControlFlowGraph.cpp
@@ -104,6 +104,29 @@ Optional<InstructionWalker> ControlFlowLoop::findInLoop(const intermediate::Inte
 	return {};
 }
 
+bool ControlFlowLoop::includes(const ControlFlowLoop& other) const
+{
+	if (*this == other) return false;
+
+	auto head = std::find_if(this->begin(), this->end(), [&](const CFGNode* node) {
+		return node->key == (*other.begin())->key;
+	});
+	if (head == this->end())
+ 	{
+		return false;
+	}
+
+	auto thisItr = head;
+	auto otherItr = other.begin();
+	while ((*thisItr)->key == (*otherItr)->key && thisItr != this->end() && otherItr != other.end())
+	{
+		++thisItr;
+		++otherItr;
+	}
+
+	return otherItr == other.end();
+}
+
 CFGNode& ControlFlowGraph::getStartOfControlFlow()
 {
 	//TODO return node without any predecessors?
@@ -129,7 +152,7 @@ CFGNode& ControlFlowGraph::getEndOfControlFlow()
 			candidate = &node;
 		}
 	}
-	
+
 	if(candidate == nullptr)
 		throw CompilationError(CompilationStep::GENERAL, "Found no CFG node without successors!");
 	return *candidate;
@@ -262,4 +285,19 @@ ControlFlowLoop ControlFlowGraph::findLoopsHelper(const CFGNode* node, FastMap<c
 	}
 
 	return loop;
+}
+
+LoopInclusionTreeNode::LoopInclusionTreeNode(const KeyType key) : Node(key) {}
+
+LoopInclusionTreeNode* LoopInclusionTreeNode::findRoot()
+{
+	for (auto &parent : this->getNeighbors())
+	{
+		if (!parent.second.includes) {
+			// The root node must be only one
+			return reinterpret_cast<LoopInclusionTreeNode*>(parent.first)->findRoot();
+		}
+	}
+	// this is root
+	return this;
 }

src/analysis/ControlFlowGraph.h
@@ -73,6 +73,11 @@ namespace vc4c
 		 * Returns the InstructionWalker for the given instruction, if it is within the loop.
 		 */
 		Optional<InstructionWalker> findInLoop(const intermediate::IntermediateInstruction* inst) const;
+
+		/*
+		 * Returns whether this loop includes other loop and doesn't equal it.
+		 */
+		bool includes(const ControlFlowLoop& other) const;
 	};
 
 	/*
@@ -118,6 +123,24 @@ namespace vc4c
 		
 		friend class Method;
 	};
+
+	/*
+	 * A relation in the control-flow-loop
+	 */
+	struct LoopInclusion
+	{
+		bool includes;
+		LoopInclusion(bool _includes) : includes(_includes) {}
+	};
+	struct LoopInclusionTreeNode : public Node<ControlFlowLoop*, LoopInclusion>
+	{
+		LoopInclusionTreeNode(const KeyType key);
+		LoopInclusionTreeNode* findRoot();
+	};
+	/*
+	 * The trees represents inclusion relation of control-flow loops. This may have multiple trees.
+	 */
+	using LoopInclusionTree = Graph<ControlFlowLoop*, LoopInclusionTreeNode>;
 
 } /* namespace vc4c */
 

src/optimization/ControlFlow.cpp
@@ -10,6 +10,8 @@
 #include "../analysis/ControlFlowGraph.h"
 #include "../analysis/DataDependencyGraph.h"
 #include "../intermediate/TypeConversions.h"
+#include "../InstructionWalker.h"
+#include "../intermediate/Helper.h"
 #include "../periphery/VPM.h"
 #include "log.h"
 
@@ -18,6 +20,7 @@
 
 using namespace vc4c;
 using namespace vc4c::optimizations;
+using namespace vc4c::intermediate;
 
 static FastSet<Local*> findLoopIterations(const ControlFlowLoop& loop, const DataDependencyGraph& dependencyGraph)
 {
@@ -664,7 +667,7 @@ static void vectorize(ControlFlowLoop& loop, LoopControl& loopControl, const Dat
 			//TODO what to do?? These are e.g. for accumulation-variables (like sum, maximum)
 			//FIXME depending on the operation performed on this locals, the vector-elements need to be folded into a scalar/previous vector width
 			logging::debug() << "Local is accessed outside of loop: " << (*openInstructions.begin())->to_string() << logging::endl;
-			
+
 			const intermediate::IntermediateInstruction* inst = *openInstructions.begin();
 			const Value& arg = inst->getArgument(0).value();
 			const intermediate::Operation* op = dynamic_cast<const intermediate::Operation*>(arg.getSingleWriter());
@@ -970,4 +973,66 @@ void optimizations::addStartStopSegment(const Module& module, Method& method, co
 	}
 
 	generateStopSegment(method);
+}
+
+void optimizations::removeConstantLoadInLoops(const Module& module, Method& method, const Configuration& config)
+{
+	// 1. find loops
+	auto &cfg = method.getCFG();
+	auto loops = cfg.findLoops();
+
+	// 2. generate inclusion relation of loops as trees
+	LoopInclusionTree inclusionTree;
+	for (auto &loop1 : loops)
+	{
+		for (auto &loop2 : loops)
+		{
+			if (loop1.includes(loop2))
+			{
+				auto &node1 = inclusionTree.getOrCreateNode(&loop1);
+				auto &node2 = inclusionTree.getOrCreateNode(&loop2);
+				node1.addNeighbor(&node2, LoopInclusion(true));
+				node2.addNeighbor(&node1, LoopInclusion(false));
+			}
+		}
+	}
+
+	// logging::debug() << "inclusionTree" << logging::endl;
+	// for (auto &loop : inclusionTree) {
+	// 	logging::debug() << "  " << loop.first << logging::endl;
+	// 	for (auto &node : loop.second.getNeighbors()) {
+	// 		logging::debug() << "    " << node.first->key << ": " << node.second.includes << logging::endl;
+	// 	}
+	// }
+
+	// 3. move constant load operations from root of trees
+	FastSet<ControlFlowLoop*> processed;
+	for (auto &loop : loops)
+	{
+		auto &node = inclusionTree.getOrCreateNode(&loop);
+		auto root = node.findRoot();
+
+		if (processed.find(root->key) != processed.end()) continue;
+		processed.insert(root->key);
+
+		for (auto &cfgNode : *root->key)
+		{
+			auto block = cfgNode->key;
+			for (auto it = block->begin(); it != block->end(); it = it.nextInBlock())
+			{
+				// TODO: Constants like `mul24 r1, 4, elem_num` should be also moved.
+				auto loadInst = it.get<LoadImmediate>();
+				if (loadInst != nullptr)
+				{
+					// LoadImmediate must have output value
+					auto out = loadInst->getOutput().value();
+					if (out.valueType == ValueType::LOCAL) {
+						auto rootInst = root->key->findPredecessor()->key->end().previousInBlock();
+						rootInst.emplace(it.release());
+						it.erase();
+					}
+				}
+			}
+		}
+	}
 }

src/optimization/ControlFlow.h
@@ -44,6 +44,11 @@ namespace vc4c
 		 */
 		void addStartStopSegment(const Module& module, Method& method, const Configuration& config);
 
+		/*
+		 * Move constant load operations in (nested) loops to the block before head block of the outer-most loop.
+		 */
+		void removeConstantLoadInLoops(const Module& module, Method& method, const Configuration& config);
+
 	} /* namespace optimizations */
 } /* namespace vc4c */
 

src/optimization/Optimizer.h
@@ -101,6 +101,8 @@ namespace vc4c
 		extern const OptimizationPass EXTEND_BRANCHES;
 		//adds the start- and stop-segments to the beginning and end of the kernel
 		extern const OptimizationPass ADD_START_STOP_SEGMENT;
+		// remove constant load in (nested) loops
+		extern const OptimizationPass REMOVE_CONSTANT_LOAD_IN_LOOPS;
 
 		/*
 		 * The default optimization passes consist of all passes listed above.

testing/test_inner_loops.cl
@@ -1,0 +1,14 @@
+/*
+ * Tests RemoveConstantLoadInLoops optimization
+ */
+__kernel void test_remove_constant_load_in_loops_opt(global float a[], global float b[]) {
+    for (int j = 0; j < 200; j++) {
+        for (int i = 0; i < 200; i++) {
+            a[i + j * 200] = i * j;
+        }
+    }
+
+    for (int i = 0; i < 40000; i++) {
+        b[i] = i;
+    }
+}

commit d961d3ebb90695ea0cb067060c8e04b514a34d2a

Fixes compilation error for LLVM 5.0+, fixes regression introduced by #56

src/llvm/BitcodeReader.cpp
@@ -32,7 +32,8 @@ using namespace vc4c::llvm2qasm;
 extern AddressSpace toAddressSpace(int num);
 extern std::string cleanMethodName(const std::string& name);
 
-static void dumpValue(const llvm::Value* val)
+template<typename T>
+static void dumpLLVM(const T* val)
 {
 #if LLVM_LIBRARY_VERSION >= 50
 	val->print(llvm::errs());
@@ -449,7 +450,7 @@ DataType BitcodeReader::toDataType(const llvm::Type* type)
 		pointerType.getPointerType().value()->addressSpace = toAddressSpace(type->getPointerAddressSpace());
 		return pointerType;
 	}
-	type->dump();
+	dumpLLVM(type);
 	throw CompilationError(CompilationStep::PARSER, "Unknown LLVM type", std::to_string(type->getTypeID()));
 }
 
@@ -1003,7 +1004,7 @@ Value BitcodeReader::toConstant(Module& module, const llvm::Value* val)
 	}
 	else
 	{
-		dumpValue(val);
+		dumpLLVM(val);
 		throw CompilationError(CompilationStep::PARSER, "Unhandled constant type", std::to_string(val->getValueID()));
 	}
 }
@@ -1032,7 +1033,7 @@ Value BitcodeReader::precalculateConstantExpression(Module& module, const llvm::
 			{
 				if(!toConstant(module, expr->getOperand(i)).isZeroInitializer())
 				{
-					dumpValue(expr);
+					dumpLLVM(expr);
 					throw CompilationError(CompilationStep::PARSER, "Only constant getelementptr without offsets are supported for now");
 				}
 			}
@@ -1161,7 +1162,7 @@ Value BitcodeReader::precalculateConstantExpression(Module& module, const llvm::
 
 	if(result)
 		return result.value();
-	dumpValue(expr);
+	dumpLLVM(expr);
 	throw CompilationError(CompilationStep::PARSER, "This type of constant expression is not supported yet", expr->getOpcodeName());
 }
 

src/optimization/Optimizer.cpp
@@ -151,7 +151,12 @@ const OptimizationPass optimizations::ADD_START_STOP_SEGMENT = OptimizationPass(
 const OptimizationPass optimizations::EXTEND_BRANCHES = OptimizationPass("ExtendBranches", extendBranches, 190);
 
 const std::set<OptimizationPass> optimizations::DEFAULT_PASSES = {
-		MAP_MEMORY_ACCESS, RUN_SINGLE_STEPS, /* SPILL_LOCALS, */ COMBINE_LITERAL_LOADS, RESOLVE_STACK_ALLOCATIONS, COMBINE_ROTATIONS, REMOVE_REDUNDANT_MOVES, ELIMINATE, VECTORIZE, SPLIT_READ_WRITES, REORDER, COMBINE, UNROLL_WORK_GROUPS, ADD_START_STOP_SEGMENT, EXTEND_BRANCHES, REMOVE_CONSTANT_LOAD_IN_LOOPS
+		MAP_MEMORY_ACCESS, RUN_SINGLE_STEPS, /* SPILL_LOCALS, */ COMBINE_LITERAL_LOADS, RESOLVE_STACK_ALLOCATIONS, COMBINE_ROTATIONS, REMOVE_REDUNDANT_MOVES, ELIMINATE, VECTORIZE, SPLIT_READ_WRITES, REORDER, COMBINE, UNROLL_WORK_GROUPS, ADD_START_STOP_SEGMENT, EXTEND_BRANCHES
+		/* , REMOVE_CONSTANT_LOAD_IN_LOOPS
+		 * TODO in combination with a bug/missing check in register-allocation, this generates invalid code (e.g. testing/test_barrier.cl)
+		 * More exact: the load is moved outside the loop but the register is re-assigned in the loop having wrong value for successive iterations
+		 * In register-allocation, need to check for loops and reserve whole loop
+		 */
 };
 
 Optimizer::Optimizer(const Configuration& config, const std::set<OptimizationPass>& passes) : config(config), passes(passes)