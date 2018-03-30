2b7ca0e88f195e72feb79517ff8d0e260cf32e0a
doe300 <stadeldani@web.de>
2018-03-30 08:47:52 +0200

    Rewrites CMake to more modern style

--- src/spirv/SPIRVHelper.h
+++ src/spirv/SPIRVHelper.h
@@ -6,11 +6,12 @@
 
 #ifndef SPIRVHELPER_H
 #define SPIRVHELPER_H
-#ifdef SPIRV_HEADER
 
-#include SPIRV_OPENCL_HEADER
-#include SPIRV_HEADER
-#include SPIRV_PARSER_HEADER
+#ifdef SPIRV_FRONTEND
+
+#include "spirv/unified1/spirv.h"
+#include "spirv/unified1/OpenCL.std.h"
+#include "spirv-tools/libspirv.hpp"
 
 #include "../Locals.h"
 
