adddcd988c2a4b648d6ec1a62472e693fd037012
long-long-float <niinikazuki@yahoo.co.jp>
2018-04-19 15:11:22 +0900

    Fix to use clang-format-5.0

--- .circleci/clang-format-diff.sh
+++ .circleci/clang-format-diff.sh
@@ -2,12 +2,13 @@
 
 RET=0
 DIR=$(readlink -f $(dirname $0)/..)
+CLANG_FORMAT=clang-format-5.0
 
 echo "Running clang-format in:"
-clang-format --version
+$CLANG_FORMAT --version
 
 for file in src/*.cpp src/*/*.cpp include/*.h src/*.h src/*/*.h; do
-    clang-format $file > /tmp/file
+    $CLANG_FORMAT $file > /tmp/file
     A=$(diff $file /tmp/file)
     if [ "$?" == 1 ] || [ "$RET" == 1 ]; then
         RET=1
