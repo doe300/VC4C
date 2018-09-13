54838db4153ca325f8681b72defd6622ed2f26a5
Ryohei Tokuda <nomaddojp362@gmail.com>
2018-09-14 01:43:34 +0900

    Fix code style

--- include/Precompiler.h
+++ include/Precompiler.h
@@ -102,7 +102,8 @@ namespace vc4c
     class Precompiler
     {
     public:
-        Precompiler(Configuration& config, std::istream& input, SourceType inputType, const Optional<std::string>& inputFile = {});
+        Precompiler(Configuration& config, std::istream& input, SourceType inputType,
+            const Optional<std::string>& inputFile = {});
 
         /*
          * Runs the pre-compilation from the source-type passed to the constructor to the output-type specified.
@@ -164,6 +165,7 @@ namespace vc4c
         const SourceType inputType;
         const Optional<std::string> inputFile;
         const Configuration config;
+
     private:
         std::istream& input;
     };
