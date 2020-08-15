c39687c9f101bfb1fa1cc4ce43187661fdaa697f
long-long-float <niinikazuki@yahoo.co.jp>
2020-08-15 16:32:29 +0900

    Create new files of ValueExpr

---
+++ src/optimization/ValueExpr.cpp
@@ -0,0 +1,195 @@
+/*
+ * Author: doe300
+ *
+ * See the file "LICENSE" for the full license governing this code.
+ */
+
+#include "ValueExpr.h"
+
+#include "../Locals.h"
+
+using namespace vc4c;
+using namespace vc4c::optimization;
+
+bool ValueBinaryOp::operator==(const ValueExpr& other) const
+{
+    if(auto otherOp = dynamic_cast<const ValueBinaryOp*>(&other))
+    {
+        return op == otherOp->op && *right == *otherOp->right && *left == *otherOp->left;
+    }
+    return false;
+}
+
+std::shared_ptr<ValueExpr> ValueBinaryOp::replaceLocal(const Value& value, std::shared_ptr<ValueExpr> expr)
+{
+    return std::make_shared<ValueBinaryOp>(left->replaceLocal(value, expr), op, right->replaceLocal(value, expr));
+}
+
+void ValueBinaryOp::expand(ExpandedExprs& exprs)
+{
+    auto leftNum = left->getInteger();
+    auto rightNum = right->getInteger();
+    if(leftNum && rightNum)
+    {
+        int l = leftNum.value_or(0);
+        int r = rightNum.value_or(0);
+        int num = 0;
+        switch(op)
+        {
+        case BinaryOp::Add:
+            num = l + r;
+            break;
+        case BinaryOp::Sub:
+            num = l - r;
+            break;
+        case BinaryOp::Mul:
+            num = l * r;
+            break;
+        case BinaryOp::Div:
+            num = l / r;
+            break;
+        case BinaryOp::Other:
+            break;
+        }
+
+        // TODO: Care other types
+        auto value = Value(Literal(std::abs(num)), TYPE_INT32);
+        std::shared_ptr<ValueExpr> expr = std::make_shared<ValueTerm>(value);
+        exprs.push_back(std::make_pair(true, expr));
+    }
+    else
+    {
+        switch(op)
+        {
+        case BinaryOp::Add:
+        {
+            left->expand(exprs);
+            right->expand(exprs);
+            break;
+        }
+        case BinaryOp::Sub:
+        {
+            left->expand(exprs);
+
+            ExpandedExprs temp;
+            right->expand(temp);
+            for(auto& e : temp)
+            {
+                e.first = !e.first;
+            }
+            exprs.insert(exprs.end(), temp.begin(), temp.end());
+            break;
+        }
+        case BinaryOp::Mul:
+        {
+            if(leftNum || rightNum)
+            {
+                int num = 0;
+                std::shared_ptr<ValueExpr> expr = nullptr;
+                if(leftNum)
+                {
+                    num = leftNum.value_or(0);
+                    expr = right;
+                }
+                else
+                {
+                    num = rightNum.value_or(0);
+                    expr = left;
+                }
+                for(int i = 0; i < num; i++)
+                {
+                    exprs.push_back(std::make_pair(true, expr));
+                }
+            }
+            else
+            {
+                exprs.push_back(std::make_pair(true, std::make_shared<ValueBinaryOp>(left, op, right)));
+            }
+            break;
+        }
+        case BinaryOp::Div:
+        {
+            exprs.push_back(std::make_pair(true, std::make_shared<ValueBinaryOp>(left, op, right)));
+            break;
+        }
+        case BinaryOp::Other:
+            break;
+        }
+    }
+}
+
+Optional<int> ValueBinaryOp::getInteger() const
+{
+    return Optional<int>();
+}
+
+std::string ValueBinaryOp::to_string() const
+{
+    std::string opStr;
+    switch(op)
+    {
+    case BinaryOp::Add:
+        opStr = "+";
+        break;
+    case BinaryOp::Sub:
+        opStr = "-";
+        break;
+    case BinaryOp::Mul:
+        opStr = "*";
+        break;
+    case BinaryOp::Div:
+        opStr = "/";
+        break;
+    case BinaryOp::Other:
+        opStr = "other";
+        break;
+    }
+
+    return "(" + left->to_string() + " " + opStr + " " + right->to_string() + ")";
+}
+
+bool ValueTerm::operator==(const ValueExpr& other) const
+{
+    if(auto otherTerm = dynamic_cast<const ValueTerm*>(&other))
+        return value == otherTerm->value;
+    return false;
+}
+
+std::shared_ptr<ValueExpr> ValueTerm::replaceLocal(const Value& from, std::shared_ptr<ValueExpr> expr)
+{
+    if(auto fromLocal = from.checkLocal())
+    {
+        if(auto valueLocal = value.checkLocal())
+        {
+            if(*fromLocal == *valueLocal)
+            {
+                return expr;
+            }
+        }
+    }
+    return std::make_shared<ValueTerm>(value);
+}
+
+void ValueTerm::expand(ExpandedExprs& exprs)
+{
+    exprs.push_back(std::make_pair(true, std::make_shared<ValueTerm>(value)));
+}
+
+Optional<int> ValueTerm::getInteger() const
+{
+    if(auto lit = value.checkLiteral())
+    {
+        return Optional<int>(lit->signedInt());
+    }
+    else if(auto imm = value.checkImmediate())
+    {
+        return imm->getIntegerValue();
+    }
+    return Optional<int>();
+}
+
+std::string ValueTerm::to_string() const
+{
+    return value.to_string();
+}
+
---
+++ src/optimization/ValueExpr.h
@@ -0,0 +1,96 @@
+/*
+ * Author: doe300
+ *
+ * See the file "LICENSE" for the full license governing this code.
+ */
+#ifndef VC4C_OPTIMIZATION_VALUEEXPR
+#define VC4C_OPTIMIZATION_VALUEEXPR
+
+#include "../Values.h"
+
+#include <vector>
+#include <memory>
+
+namespace vc4c
+{
+    namespace optimization
+    {
+        class ValueExpr
+        {
+        public:
+            // (signed, value)
+            using ExpandedExprs = std::vector<std::pair<bool, std::shared_ptr<ValueExpr>>>;
+
+            virtual ~ValueExpr() = default;
+
+            virtual bool operator==(const ValueExpr& other) const = 0;
+            inline bool operator!=(const ValueExpr& other) const
+            {
+                return !(*this == other);
+            }
+
+            virtual std::shared_ptr<ValueExpr> replaceLocal(const Value& value, std::shared_ptr<ValueExpr> expr) = 0;
+
+            // expand value expr as liner combination
+            // e.g. (a + b) * c = a * c + b * c
+            virtual void expand(ExpandedExprs& exprs) = 0;
+
+            virtual Optional<int> getInteger() const = 0;
+
+            virtual std::string to_string() const = 0;
+        };
+
+        class ValueBinaryOp : public ValueExpr
+        {
+        public:
+            enum class BinaryOp
+            {
+                Add,
+                Sub,
+                Mul,
+                Div,
+                Other,
+            };
+
+            ValueBinaryOp(std::shared_ptr<ValueExpr> left, BinaryOp op, std::shared_ptr<ValueExpr> right) :
+                left(left), op(op), right(right)
+            {
+            }
+
+            bool operator==(const ValueExpr& other) const override;
+
+            std::shared_ptr<ValueExpr> replaceLocal(const Value& value, std::shared_ptr<ValueExpr> expr) override;
+
+            void expand(ExpandedExprs& exprs) override;
+
+            Optional<int> getInteger() const override;
+
+            std::string to_string() const override;
+
+            std::shared_ptr<ValueExpr> left;
+            BinaryOp op;
+            std::shared_ptr<ValueExpr> right;
+        };
+
+        class ValueTerm : public ValueExpr
+        {
+        public:
+            ValueTerm(const Value& value) : value(value) {}
+
+            bool operator==(const ValueExpr& other) const override;
+
+            std::shared_ptr<ValueExpr> replaceLocal(const Value& from, std::shared_ptr<ValueExpr> expr) override;
+
+            void expand(ExpandedExprs& exprs) override;
+
+            Optional<int> getInteger() const override;
+
+            std::string to_string() const override;
+
+            const Value value;
+        };
+
+    } /* namespace optimizations */
+} /* namespace vc4c */
+
+#endif /* VC4C_OPTIMIZATION_VALUEEXPR */
